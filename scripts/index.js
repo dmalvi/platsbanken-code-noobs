c4aff2afad02240c743e3794e067dc8ad4c6dc38
dmalvi <dmalvi@gmail.com>
2018-06-27 16:09:12 +0200

    added pagination including button

--- scripts/index.js
+++ scripts/index.js
@@ -1,46 +1,63 @@
 
 const searchButton = document.querySelector(".searchButton");
-let searchCriteria = "platsannonser/matchning?lanid=3&yrkesomradeid=3&antalrader=30";
+let searchCriteria = "platsannonser/matchning?lanid=3&yrkesomradeid=3&antalrader=30&sida=";
 const sthlmButton = document.querySelector("#stockholm");
 const gbgButton = document.querySelector("#goteborg");
 const uppsalaButton = document.querySelector("#uppsala");
 const malmoButton = document.querySelector("#malmo");
 const printDiv = document.querySelector("#print");
+const nextButton = document.querySelector(".next");
+let pageNumber = 1;
+const baseURL = 'http://api.arbetsformedlingen.se/af/v0/';
 
+// function for when you push SÖK
 searchButton.addEventListener("click", async function() {
   printDiv.innerHTML = "";
   if (sthlmButton.checked) {
-    searchCriteria = "platsannonser/matchning?lanid=1&yrkesomradeid=3&antalrader=30";
+    searchCriteria = "platsannonser/matchning?lanid=1&yrkesomradeid=3&antalrader=30&sida=";
   } else if (uppsalaButton.checked) {
-    searchCriteria = "platsannonser/matchning?lanid=3&yrkesomradeid=3&antalrader=30";
+    searchCriteria = "platsannonser/matchning?lanid=3&yrkesomradeid=3&antalrader=30&sida=";
   } else if (gbgButton.checked) {
-    searchCriteria = "platsannonser/matchning?lanid=14&yrkesomradeid=3&antalrader=30";
+    searchCriteria = "platsannonser/matchning?lanid=14&yrkesomradeid=3&antalrader=30&sida=";
   } else if (malmoButton.checked) {
-    searchCriteria = "platsannonser/matchning?lanid=12&yrkesomradeid=3&antalrader=30";
+    searchCriteria = "platsannonser/matchning?lanid=12&yrkesomradeid=3&antalrader=30&sida=";
   } else {
-    searchCriteria = "platsannonser/matchning?yrkesomradeid=3&antalrader=80";
+    searchCriteria = "platsannonser/matchning?yrkesomradeid=3&antalrader=30&sida=";
   }
-  const baseURL = 'http://api.arbetsformedlingen.se/af/v0/';
-  const responseObject = await fetch(baseURL + searchCriteria);
+  const responseObject = await fetch(baseURL + searchCriteria + pageNumber);
   let matches = await responseObject.json();
   let matchningdata = matches.matchningslista.matchningdata;
   let printString = getHtmlString(matchningdata);
-  printJobs(printString); 
-  });
+  printJobs(printString);
+});
 
-  function printJobs(string) {
-    printDiv.insertAdjacentHTML("beforeend", string);
-  }
+// function for when you push LADDA FLER
+nextButton.addEventListener("click",async function(){
+  pageNumber = pageNumber + 1;
+  searchCriteria = "platsannonser/matchning?yrkesomradeid=3&antalrader=30&sida=";
+  const responseObject = await fetch(baseURL + searchCriteria + pageNumber);
+  let matches = await responseObject.json();
+  let matchningdata = matches.matchningslista.matchningdata;
+  let printString = getHtmlString(matchningdata);
+  printJobs(printString);
+});
 
+//prints out HTMLstring on the page
+function printJobs(string) {
+  printDiv.insertAdjacentHTML("beforeend", string);
+}
 
-  function getHtmlString(data){
+//what to print out
+function getHtmlString(data){
   let htmlString = "";
   for(let post of data){
     htmlString +=`
     <div class="card" id="${post.annonsid}">
+    <p>${post.annonsid}</p>
     <h2>${post.annonsrubrik}</h2>
     <p>${post.yrkesbenamning}</p>
     <p>${post.arbetsplatsnamn}</p>
+    <a href="${post.annonsurl}" target="_blank">Läs mer</a>
     </div>
     `;
   };
